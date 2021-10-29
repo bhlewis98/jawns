a3ccd6d7affd84ff0fb28bfe32304e7b4ea6f660
Benjamin Lewis <bhlewis@iu.edu>
2021-10-28 21:57:45 -0400

    adding edit and delete functionality

--- app/public/js/books.js
+++ app/public/js/books.js
@@ -20,8 +20,26 @@ const Books = {
                 console.error(err);
             })
         },
-        postNewBook(evt) {  
-            fetch('api/book/create.php', {
+        selectBook(b) {
+            if (b == this.selectedBook) {
+                return;
+            }
+            this.selectedBook = b;
+        },
+        postBook(evt) {
+            console.log ("Test:", this.selectedBook);
+          if (this.selectedBook) {
+              this.postEditBook(evt);
+          } else {
+              this.postNewBook(evt);
+          }
+        },
+        postEditBook(evt) {
+            this.bookForm.id = this.selectedBook.id;       
+            
+            console.log("Editing!", this.bookForm);
+    
+            fetch('api/book/update.php', {
                 method:'POST',
                 body: JSON.stringify(this.bookForm),
                 headers: {
@@ -35,9 +53,60 @@ const Books = {
                 this.books = json;
                 
                 // reset the form
-                this.bookForm = {};
+                this.handleResetEdit();
               });
+        },
+        postNewBook(evt) {  
+            fetch('api/book/create.php', {
+                method:'POST',
+                body: JSON.stringify(this.bookForm),
+                headers: {
+                  "Content-Type": "application/json; charset=utf-8"
+                }
+              })
+              .then( response => response.json() )
+              .then( json => {
+                console.log("Returned from post:", json);
+                // TODO: test a result was returned!
+                this.books = json;
+              });
+            // reset the form
+            this.bookForm = {};
+            this.fetchBookData();
+        },
+        postDeleteBook(b) {  
+            if ( !confirm("Are you sure you want to delete the book " + b.title + "?") ) {
+                return;
+            }  
+            
+            console.log("Delete!", b);
+    
+            fetch('api/book/delete.php', {
+                method:'POST',
+                body: JSON.stringify(b),
+                headers: {
+                  "Content-Type": "application/json; charset=utf-8"
+                }
+              })
+              .then( response => response.json() )
+              .then( json => {
+                console.log("Returned from post:", json);
+                // TODO: test a result was returned!
+                this.books = json;
+                
+                // reset the form
+                this.handleResetEdit();
+              });
+        },
+        handleEditBook(book) {
+            this.selectedBook = book;
+            this.bookForm = Object.assign({}, this.selectedBook);
+        },
+        handleResetEdit() {
+            this.selectedBook = null;
+            this.bookForm = {};
         }
+        
 
     },
     created() {
@@ -46,4 +115,4 @@ const Books = {
     }
 }
   
-Vue.createApp(Books).mount('#booksApp');
+Vue.createApp(Books).mount('#bookApp');
